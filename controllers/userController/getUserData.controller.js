4f5d2cd11fe5fcde0c44d0ba948be84f4dd2140b
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2025-08-10 15:49:42 +0530

    Add mock data

--- controllers/userController/getUserData.controller.js
+++ controllers/userController/getUserData.controller.js
@@ -1,12 +1,12 @@
 import { asyncHandler, ApiResponse } from "../../utils/index.js";
 
 export const getUserData = asyncHandler(async(req, res) => {
-    const user = req.user;
+    // const user = req.user;
 
     return res.status(200).json(
         new ApiResponse(
             200,
-            user,
+            {id: 2323, name: "John Doe"},
             "User Data retrived successfully."            
         )
     );
