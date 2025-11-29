3a9633647c119ed40e3f002d5b8ebf086606dbb1
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2025-11-29 20:43:15 +0530

    Add error log

--- controllers/userController/signinUser.controller.js
+++ controllers/userController/signinUser.controller.js
@@ -16,65 +16,73 @@ export const signinUser = asyncHandler(async(req, res) => {
         });
     }
 
-    const user = await prismaClient.user.findFirst({
-        where: {
-            username
-        }
-    });
-
-    if(!user) {
-        return res.status(404).json({
-            message: "User does not exist with this username.",
-            status: false
+    try {
+        const user = await prismaClient.user.findFirst({
+            where: {
+                username
+            }
         });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user?.password);
-
-    if(!isPasswordValid) {
-        return res.status(401).json({
-            message: "Password is not valid.",
-            status: false
-        });
-    }
-
-    const { accessToken, refreshToken } = generateAccessAndRefreshTokens(user);
-
-    const loggedInUser = await prismaClient.user.update({
-        where: {
-            id: user.id
-        },
-        data: {
-            refreshToken: refreshToken
-        },
-        select: {
-            id: true,
-            name: true,
-            picture: true,
-            email: true,
-            username: true,
-            dob: true,
-            totalBids: true
+    
+        if(!user) {
+            return res.status(404).json({
+                message: "User does not exist with this username.",
+                status: false
+            });
         }
-    })
-
-    const options = {
-        httpOnly: true,
-        sameSite: 'none',
-        secure: true
-    }
-
-    return res
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", refreshToken, options)
-    .json(
-        new ApiResponse(
-            200,
-            {
-                user: loggedInUser, accessToken, refreshToken
+    
+        const isPasswordValid = await bcrypt.compare(password, user?.password);
+    
+        if(!isPasswordValid) {
+            return res.status(401).json({
+                message: "Password is not valid.",
+                status: false
+            });
+        }
+    
+        const { accessToken, refreshToken } = generateAccessAndRefreshTokens(user);
+    
+        const loggedInUser = await prismaClient.user.update({
+            where: {
+                id: user.id
             },
-            "User logged In Successfuly"
-        )
-    );
+            data: {
+                refreshToken: refreshToken
+            },
+            select: {
+                id: true,
+                name: true,
+                picture: true,
+                email: true,
+                username: true,
+                dob: true,
+                totalBids: true
+            }
+        })
+    
+        const options = {
+            httpOnly: true,
+            sameSite: 'none',
+            secure: true
+        }
+    
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser, accessToken, refreshToken
+                },
+                "User logged In Successfuly"
+            )
+        );
+    } catch (error) {
+        console.error("Error signing in user:", error.message);
+        return res.status(500).json({
+            message: error.message || "Internal Server Error",
+            status: false
+        });
+    }
 })
